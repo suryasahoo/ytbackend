6c6bd8a4df1edacf5d87300a0e0f23f95db9ca75
suryasahoo <suryasahoo-123@outlook.com>
2024-01-15 15:03:46 +0530

    connect to MongoDB database

---
+++ src/index.js
@@ -0,0 +1,61 @@
+// require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({ path: './env' })
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+(async() => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log('Error connecting to database:', error)
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("Error: ", error);
+        throw err
+    }
+})()*/
